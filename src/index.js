8b85bf55166ab9170e7262d46376752c37a236a0
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-26 18:36:36 +0530

    Added get user and task by id api route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose'); // By using require, we ensure that the file db/mongoo
 const User = require('./models/user');
 const Task = require('./models/task');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,7 +14,7 @@ app.use(express.json());
 app.post('/users',(req,res)=>{
     const user = new User(req.body);
     user.save()
-    .then(response => res.send(user))
+    .then(response => res.status(201).send(user))
     .catch(error => {
         res.status(400).send(error.message);
     })
@@ -22,21 +23,45 @@ app.post('/users',(req,res)=>{
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body);
     task.save()
-    .then(response=> res.send(response))
+    .then(response=> res.status(201).send(response))
     .catch(error=> res.status(400).send(error.message));
 })
 
-
 app.get('/users', (req,res)=>{
     User.find({})
-    .then(queryResult=> res.send(queryResult))
+    .then(users=> res.status(200).send(users))
     .catch(error=> {res.status(500).send(error)})
+})
+
+app.get('/users/:id', (req,res)=>{
+    const _id = req.params.id
+    User.findById(_id)
+    .then(user=> {
+        if(!user){
+            return res.status(404).send({error: 'User does not exist'});    
+        }
+        res.status(200).send(user)})
+    .catch(error => res.status(500).send(error.message))
 })
 
 app.get('/tasks', (req, res)=>{
     Task.find({})
-    .then(queryResult=> res.send(queryResult))
-    .catch(error=>res.status(400).send(error));
+    .then(queryResult=> res.status(200).send(queryResult))
+    .catch(error=>res.status(500).send(error));
+});
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id;
+    Task.findById(_id)
+    .then(task=>{
+        if(!task){
+            return res.status(404).send({error: "Task does not exist."})
+        }
+        res.send(task);
+    })
+    .catch(error => {
+        res.status(500).send(error.message);
+    })
 })
 
 app.listen(port,()=>{
