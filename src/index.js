59d2c002fc083f4fce3c676ba93cafbc546a444a
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-27 14:05:45 +0530

    refractored mongoose promise consumption to async/await

--- src/index.js
+++ src/index.js
@@ -11,57 +11,71 @@ const port = process.env.PORT || 3000;
 //incoming json to an object so we can access it in our request
 app.use(express.json());
 
-app.post('/users',(req,res)=>{
-    const user = new User(req.body);
-    user.save()
-    .then(response => res.status(201).send(user))
-    .catch(error => {
-        res.status(400).send(error.message);
-    })
-})
 
-app.post('/tasks',(req,res)=>{
-    const task = new Task(req.body);
-    task.save()
-    .then(response=> res.status(201).send(response))
-    .catch(error=> res.status(400).send(error.message));
-})
-
-app.get('/users', (req,res)=>{
-    User.find({})
-    .then(users=> res.status(200).send(users))
-    .catch(error=> {res.status(500).send(error)})
-})
-
-app.get('/users/:id', (req,res)=>{
-    const _id = req.params.id
-    User.findById(_id)
-    .then(user=> {
-        if(!user){
-            return res.status(404).send({error: 'User does not exist'});    
-        }
-        res.status(200).send(user)})
-    .catch(error => res.status(500).send(error.message))
-})
-
-app.get('/tasks', (req, res)=>{
-    Task.find({})
-    .then(queryResult=> res.status(200).send(queryResult))
-    .catch(error=>res.status(500).send(error));
+app.post('/users', async (request, response)=> {
+    const user = new User(request.body);
+    try {
+        const userResponse = await user.save();
+        response.status(201).send(userResponse);
+    } catch (error) {
+        response.status(400).send({error : error.message});
+    } 
 });
 
-app.get('/tasks/:id',(req,res)=>{
-    const _id = req.params.id;
-    Task.findById(_id)
-    .then(task=>{
-        if(!task){
-            return res.status(404).send({error: "Task does not exist."})
+app.get('/users',async (request, response)=>{
+    try {
+        const users = await User.find({});
+        response.status(200).send(users);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+})
+
+
+app.get('/users/:id', async (request, response)=> {
+    const _id = request.params.id;
+    try {
+        const user = await User.findById(_id);
+        if(!user){
+            return response.status(404).send({error: 'User does not exist'});    
         }
-        res.send(task);
-    })
-    .catch(error => {
-        res.status(500).send(error.message);
-    })
+        response.status(200).send(user);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+})
+
+
+app.post('/tasks', async (request, response)=>{
+    const task = new Task(request.body);
+    try {
+        const taskResponse = await task.save();
+        response.status(201).send(taskResponse);
+    } catch (error) {
+        response.status(400).send({error: error.message});
+    }
+})
+
+app.get('/tasks', async (request, response)=>{
+    try {
+        const tasks = await Task.find({});
+        response.status(200).send(tasks);
+    } catch (error) {
+        response.status(500).send({error: error.message});
+    }
+})
+
+app.get('/tasks/:id',async (request, response)=>{
+    const _id = request.params.id;
+    try {
+        const task = await Task.findById(_id);
+        if(!task){
+            return response.status(404).send({error: "Task does not exist."})
+        }
+        response.status(200).send(task);
+    } catch (error) {
+        response.status(400).send({error: error.message});
+    }
 })
 
 app.listen(port,()=>{
