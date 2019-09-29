c1a9ab5dd03295bc6d4ee0ac9552c48c7764ec65
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-30 02:44:45 +0530

    authenticating tasks endpoints

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,40 +17,38 @@ router.post('/tasks',auth, async (request, response)=>{
     }
 })
 
-router.get('/tasks', async (request, response)=>{
+router.get('/tasks', auth,async (request, response)=>{
     try {
-        const tasks = await Task.find({});
-        response.status(200).send(tasks);
+        const tasks = await Task.find({author: request.user._id});
+        response.status(200).send(tasks);   
     } catch (error) {
         response.status(500).send({error: error.message});
     }
 })
 
-router.get('/tasks/:id',async (request, response)=>{
+router.get('/tasks/:id',auth, async (request, response)=>{
     const _id = request.params.id;
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({_id, author:request.user._id});
         if(!task){
             return response.status(404).send({error: "Task does not exist."})
         }
         response.status(200).send(task);
     } catch (error) {
-        response.status(400).send({error: error.message});
+        response.status(500).send({error: error.message});
     }
 })
 
-router.patch('/tasks/:id',async (request, response)=> {
+router.patch('/tasks/:id',auth, async (request, response)=> {
     const _id = request.params.id;
     const allowedUpdateFields = ['description','completed'];
     const requestedUpdateFields = Object.keys(request.body);
+    const isValidOperation = requestedUpdateFields.every(field=> allowedUpdateFields.includes(field));
 
-    requestedUpdateFields.forEach(field => {
-        if(!allowedUpdateFields.includes(field)){
-            return response.status(400).send({error: `Either Tasks does not have ${field} field or it can not be updated.`});
-        }
-    });
+    if(!isValidOperation)
+        return response.status(400).send({error: 'Invalid updates!'});
     try {   
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({_id,author: request.user._id});
         if(!task){
             return response.status(404).send({error: 'Task does not exist'});
         }
@@ -64,14 +62,14 @@ router.patch('/tasks/:id',async (request, response)=> {
     }
 })
 
-router.delete('/tasks/:id', async (request, response) => {
-    const id = request.params.id;
+router.delete('/tasks/:id',auth, async (request, response) => {
+    const _id = request.params.id;
     try {
-        const deletedUser = await Task.findByIdAndDelete(id);
-        if(!deletedUser){
-            return response.status(404).send({error: `Task with id ${id} does not exist.`});
+        const deletedTask = await Task.findOneAndDelete({_id, author: request.user._id});
+        if(!deletedTask){
+            return response.status(404).send({error: `Task does not exist.`});
         }
-        return response.status(200).send(deletedUser);
+        return response.status(200).send(deletedTask);
     } catch (error) {
         response.status(400).send({error: error.message});
     }
