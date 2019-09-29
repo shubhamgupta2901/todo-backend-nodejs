ed2cea1a2b5d78d9124e55a9b8ede92223605b95
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-30 00:38:23 +0530

    authenticating user endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,8 +27,7 @@ router.post('/users/login', async (request, response)=> {
         const token = await user.generateAuthToken();
         response.status(200).send({token,user});
     } catch (error) {
-        console.log(error);
-        response.status(400).send({error: error});
+        response.status(400).send({error: error.message});
     }
 })
 
@@ -73,20 +72,7 @@ router.get('/users/me',auth,async (request, response)=>{
     }
 })
 
-router.get('/users/:id', async (request, response)=> {
-    const _id = request.params.id;
-    try {
-        const user = await User.findById(_id);
-        if(!user){
-            return response.status(404).send({error: 'User does not exist'});    
-        }
-        response.status(200).send(user);
-    } catch (error) {
-        response.status(500).send({error: error.message});
-    }
-})
-
-router.patch('/users/:id',async (request, response)=>{
+router.patch('/users/me',auth,async (request, response)=>{
     const allowedUpdateFields = ['name', 'email', 'password', 'age'];
     const requestedUpdateFields = Object.keys(request.body);
 
@@ -94,36 +80,24 @@ router.patch('/users/:id',async (request, response)=>{
         if(!allowedUpdateFields.includes(field))
             response.status(400).send({error: `Either Users do not have ${field} field or it can not be updated`});
     })
-
-    const _id = request.params.id;
     try {
-        //the findByIdAndUpdate bypasses mongoose and performs operation directly on mongodb database, because of this our middleware for hashing password does not run
-        //const user = await User.findByIdAndUpdate(_id,request.body,{new: true, runValidators: true});
-        const user = await User.findById(_id);
-        if(!user){
-            return response.status(404).send({error: 'User does not exist'})
-        }
+        const user = request.user;
         requestedUpdateFields.forEach(field => {
             user[field] = request.body[field];
         })
         await user.save();
         response.status(200).send(user);
     } catch (error) {
-        response.status(400).send({error: error.message});
+        response.status(500).send({error: error.message});
     }
 })
 
-router.delete('/users/:id', async (request, response)=>{
-    const _id = request.params.id;
+router.delete('/users/me',auth, async (request, response)=>{
     try {
-        const res = await User.deleteOne({_id});
-        if(res.ok === 0)
-            return response.status(400).send({error: "Could not perform delete."})
-        else if(res.deletedCount === 0)
-            return response.status(400).send({error: `User with id: ${_id} does not exist.`})
-        response.status(200).send(res);
+        await request.user.remove();
+        response.status(200).send(request.user);
     } catch (error) {
-        response.status(400).send({error: error.message});
+        response.status(500).send({error: error.message});
     }
 })
 
