de15b848b003ed0e11f1ab8417f12fcaa40bd8ca
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-10-07 13:32:27 +0530

    adding landing page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('./db/mongoose'); // By using require, we ensure that the file db/mongoose.js runs which will ensure that mongoose connects to database.
 const UserRouter = require('./routers/user');
@@ -10,6 +11,8 @@ const port = process.env.PORT;
  * When we set up this line, node will automatically parse the incoming json to an object so we can access it in our request
  */
 app.use(express.json());
+const publicDirectoryPath = path.join(__dirname,'../public');
+app.use(express.static(publicDirectoryPath));
 
 /**
  * route requests to respective route handlers
